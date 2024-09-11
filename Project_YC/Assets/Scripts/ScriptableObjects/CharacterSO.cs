4c91a34de5707df0c707801a4cc65a62029d228d
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2024-09-11 18:15:21 +0900

    refactor: 불필요한 참조 삭제 및 디렉토리 접근 방식 변경

--- Project_YC/Assets/Scripts/ScriptableObjects/CharacterSO.cs
+++ Project_YC/Assets/Scripts/ScriptableObjects/CharacterSO.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObject/CharacterData", order = int.MinValue)]
--- Project_YC/Assets/Scripts/Utils/Values.cs
+++ Project_YC/Assets/Scripts/Utils/Values.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Values
 {
     public class Directory
     {
-
+        public const string DIR_DATA_CHARACTER = "Datas/Characters";
+        public const string DIR_DATA_DIALOGUE = "Datas/Dialogues";
+        public const string DIR_DATA_ITEM = "Datas/Items";
     }
 }
