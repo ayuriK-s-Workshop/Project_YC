2426c8e7bc659ef66012b842565740f67ba1643f
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2024-10-16 16:29:45 +0900

    feat: 창고 UI와 실제 창고 데이터 연동

--- Project_YC/Assets/Scripts/Controller/Scene/ResistanceManageController.cs
+++ Project_YC/Assets/Scripts/Controller/Scene/ResistanceManageController.cs
@@ -1,9 +1,40 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResistanceManageController : SceneController
 {
+    private GameObject _storageUI;
+    private GameObject _storageItemPrefab;
+
     void Start()
     {
-        
+        // StorageUI Initialize
+        {
+            _storageUI = GameObject.Find("MainCanvas/StorageUI");
+            _storageItemPrefab = Resources.Load("Prefabs/UI/Item") as GameObject;
+            Transform storageInstancePoint  = _storageUI.transform.Find("ScrollView/Viewport/Content");
+            foreach (InterchangeableItemSO item in Manager.Game.playerStorage)
+            {
+                GameObject go = Instantiate(_storageItemPrefab, storageInstancePoint);
+                go.transform.Find("Image").GetComponent<Image>().sprite = item.texture;
+                go.transform.Find("Count").GetComponent<TextMeshProUGUI>().text = "1";
+            }
+            _storageUI.SetActive(false);
+        }
+
+        interactableObject += ObjectInteractHandler;
+    }
+
+    override protected void ObjectInteractHandler(UnityEngine.Object obj)
+    {
+        switch (obj.name)
+        {
+            case "Storage":
+                {
+                    _storageUI.SetActive(true);
+                    break;
+                }
+        }
     }
 }
--- Project_YC/Assets/Scripts/Controller/Scene/SceneController.cs
+++ Project_YC/Assets/Scripts/Controller/Scene/SceneController.cs
@@ -4,4 +4,6 @@ using UnityEngine;
 public class SceneController : MonoBehaviour
 {
     public Action<UnityEngine.Object> interactableObject;
+
+    virtual protected void ObjectInteractHandler(UnityEngine.Object obj) { }
 }
